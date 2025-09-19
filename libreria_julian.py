9bc57a8b398f70503fea62d785afa25b16ce0b91
Juliancho <jroblesfarias@gmail.com>
2025-09-19 11:32:14 -0500

    Se realiza mi avance de practiva de tratammiendo de bases de datos.

--- libreria_julian.py
+++ libreria_julian.py
@@ -1,1 +1,11 @@
-a
+import pandas as pd  # se importa la librería pandas y se le da el alias pd
+
+class jrlibrerias:
+    def leer_excel(self, path):
+        """
+        Me lee y me devuelve un DataFrame.
+        - self: referencia a la propia instancia del objeto.
+        - path: ruta del archivo a leer (string o Path).
+        """
+        df = pd.read_excel(path)  # lee un archivo y devuelve un DataFrame
+        return df  # devuelve el DataFrame a quien lo llamó
